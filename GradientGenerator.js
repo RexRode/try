826eba4bdac943c48e1e6ceddcdcbe7cdcf09290
RexRode <peterburbery1@gmail.com>
2021-02-09 14:17:58 -0500

    On branch main Changes to be committed:

--- GradientGenerator.js
+++ GradientGenerator.js
@@ -3,7 +3,7 @@ var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
-
+//this is a comment
 function setGradient() {
     body.style.background =
         "linear-gradient(to right, "
